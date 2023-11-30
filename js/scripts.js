ed99b071f31e98bbd04c465a00f02a1c35a8a4a3
MirkoCifelli <ciffo4@hotmail.it>
2023-11-30 14:17:41 +0100

    Add some content

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+const text = prompt('Dimmi il tuo nome:');
+console.log(typeof text);
+console.log('Valore preso dal prompt:', text);
+
+console.log(`
+    L'utente ha inserito:
+    ${text}
+`);
+
+const surname = prompt('Dimmi il tuo cognome:');
+console.log(typeof text);
+console.log('Valore preso dal prompt:', text);
+
+console.log(`
+    L'utente ha inserito:
+    ${surname}
+`);
+
+const color = prompt('Dimmi il tuo colore preferito:');
+console.log(typeof text);
+console.log('Valore preso dal prompt:', text);
+
+console.log(`
+    L'utente ha inserito:
+    ${color}
+`);
+
+const fissa = 114
+
+const miaStringa = text + surname + color +  fissa
+document.writeln(miaStringa);
